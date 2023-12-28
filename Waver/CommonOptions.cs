b735a4015b9e6b2d3da6c3a0d1824c7cf2ab5fc0
Wolfgang Wallhäuser <wolfgang@wallhaeuser.eu>
2023-12-28 18:16:03 +0100

    Default to sample rate 16 kHz

--- Waver/CommonOptions.cs
+++ Waver/CommonOptions.cs
@@ -25,7 +25,7 @@ public class CommonOptions
     [Option('m', "mono", Required = false, HelpText = "Output to mono file", Default = false)]
     public bool Mono { get; set; }
     
-    [Option('s', "sample_rate", Required = false, HelpText = "Output sample rate in Hz", Default = 48000)]
+    [Option('s', "sample_rate", Required = false, HelpText = "Output sample rate in Hz", Default = 16000)]
     public int SampleRate { get; set; }
     
     [Option('n', "normalization", Required = false, HelpText = "Normalize output volume to max peek db level, e.g. -3", Default = null)]
