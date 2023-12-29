ad01ea4a40d1d1338f982dce2fa87508d51ab592
Wolfgang Wallhäuser <wolfgang@wallhaeuser.eu>
2023-12-29 09:33:54 +0100

    moved methods to base class when no split count given, default to max split count (whole file)

--- Waver/ResampleBase.cs
+++ Waver/ResampleBase.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.RegularExpressions;
 using Microsoft.VisualBasic;
 using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
 
 namespace Waver;
 
@@ -114,5 +115,87 @@ public abstract class ResampleBase
         var waveFormat = waveReader.WaveFormat;
         Console.WriteLine($"{fileName}: {waveFormat.Channels} channel(s), {waveFormat.SampleRate} Hz, {waveFormat.BitsPerSample} bit/sample, {waveFormat.Encoding}");
         
+    }
+
+    protected static void SplitWaveFile(string inputFileName, string outputFileName, ISampleProvider sampleProvider, WaveOptions waveOptions, TimeSpan totalDuration)
+    {
+        var splitDuration = TimeSpan.FromSeconds(waveOptions.SplitDuration ?? 10);
+        var maxSplitCount = (int)Math.Ceiling(totalDuration.TotalSeconds / splitDuration.TotalSeconds);
+        var splitCount = Math.Min(waveOptions.SplitCount ?? maxSplitCount, maxSplitCount);
+
+        for (var count = 0; count < splitCount; count++)
+        {
+            var extension = Path.GetExtension(outputFileName);
+            var splitFileName = outputFileName.Replace(extension, $"_{count}{extension}");
+            WriteWaveFile(inputFileName, splitFileName, sampleProvider.Take(splitDuration), waveOptions);
+        }
+    }
+
+    protected static void WriteWaveFile(string inputFileName, string outputFileName, ISampleProvider sampleProvider, CommonOptions resampleOptions)
+    {
+        WaveFileWriter.CreateWaveFile16(outputFileName, sampleProvider);
+        
+        Console.WriteLine($"{inputFileName} -> {outputFileName}");
+
+        if (!resampleOptions.Verbose) return;
+        
+        PrintWaveFileFormat(outputFileName);
+    }
+
+    protected static ISampleProvider GetResamplingProvider(ISampleProvider reader, WaveOptions resampleOptions)
+    {
+        var resamplingSampleProvider = new WdlResamplingSampleProvider(reader, resampleOptions.SampleRate);
+
+        if (reader.WaveFormat.Channels == 2 && resampleOptions.Mono)
+        {
+            return new StereoToMonoSampleProvider(resamplingSampleProvider);
+        }
+
+        return resamplingSampleProvider;
+    }
+
+    protected static void ApplyNormalization(AudioFileReader reader, WaveOptions waveOptions)
+    {
+        switch (waveOptions.Nomalization)
+        {
+            case null:
+                return;
+            case > 0:
+                Console.WriteLine($"Skip normalization, invalid db level {waveOptions.Nomalization.Value}, must be <= 0.");
+                return;
+        }
+
+        // find the max peak
+        var buffer = new float[reader.WaveFormat.SampleRate];
+        int read;
+        var maxSampleValue = 0.0f;
+        do
+        {
+            read = reader.Read(buffer, 0, buffer.Length);
+            for (var n = 0; n < read; n++)
+            {
+                var abs = Math.Abs(buffer[n]);
+                if (abs < maxSampleValue) continue;
+                maxSampleValue = abs;
+            }
+        } while (read > 0);
+
+        reader.Position = 0;
+
+        if (maxSampleValue is > 0.0f and < 1.0f)
+        {
+            var maxRequestedSampleValue = Math.Pow(10, waveOptions.Nomalization.Value / 20f);
+            var volumeFactor = (float)maxRequestedSampleValue / maxSampleValue;
+                
+            reader.Volume = volumeFactor;
+                
+            if (waveOptions.Verbose)
+                Console.WriteLine($"Max sample value: {maxSampleValue}, {waveOptions.Nomalization.Value} dB = {maxRequestedSampleValue} -> set volume to {volumeFactor}");
+        }
+        else
+        {
+            if (waveOptions.Verbose)
+                Console.WriteLine($"Max sample value: {maxSampleValue}, do not normalize");
+        }
     }
 }
